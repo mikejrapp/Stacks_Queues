04fc6c246f9862e687e28677413e376cb57cd46e
Michael <mikejrapp@gmail.com>
2016-10-01 02:24:42 -0600

    string parse working for stack

--- StackAndQueue/StackAndQueue.cpp
+++ StackAndQueue/StackAndQueue.cpp
@@ -8,15 +8,19 @@ using namespace std;
 
 void test(Stack<int> &pStack);
 void testQ(Queue<int> &pQueue);
+void readFile(Stack<int> &pStack, Queue<int> &pQueue, ifstream &inFile);
 
 int main() {
 	
 	Stack<int> stack;
 	Queue<int> queue;
+	ifstream inFile;
 
-	test(stack);
-	testQ(queue);
+	/*test(stack);
+	testQ(queue);*/
+	readFile(stack, queue, inFile);
 
+	cin.ignore();
 	cin.get();
 	return 0;
 }
@@ -55,4 +59,34 @@ void testQ(Queue<int> &pQueue) {
 	cout << pQueue.serve() << endl;
 	cout << "Serving next item: ";
 	cout << pQueue.serve() << endl;
+}
+
+void readFile(Stack<int> &pStack, Queue<int> &pQueue, ifstream &inFile) {
+	string filepath;
+	string line;
+	string data;
+	int iData;
+	const int OFFSET = 2; //used as the offset for parsing the string. Finds last character of command, then moves to first pos of data.
+	cout << "Please enter the file path including the file extension: ";
+	cin >> filepath;
+	inFile.open(filepath);
+
+
+	if (!inFile) {
+		cout << "File not found. Program terminates.";
+	}
+	else {
+		while (!inFile.eof()) {
+			getline(inFile, line);
+			if (line.find("push") != string::npos) {
+				data = line.substr(line.find_first_of("h") + OFFSET, line.length() - 1);
+				iData = stoi(data);
+				cout << "Pushing " << data << " onto stack." << endl;
+				pStack.push(iData);
+			}//end if
+			if (line.find("pop") != string::npos) {
+				cout << "Popping " << pStack.pop() << " off of stack" << endl;
+			}
+		}//end while
+	}//end if !inFile
 }
